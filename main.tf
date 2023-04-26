4ffa2a5b5d7843b035abaa6966aacf9f45476358
anupama <anupama@gmail.com>
2023-04-25 23:41:13 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
   source         = "./vendor/modules/vpc"
   CIDR_VPC       = "var.CIDR_VPC"
+  ENV             = var.ENV
 }
 
 # This always goes with the main branch, so how can I tell this module pick the code from the intended branch
